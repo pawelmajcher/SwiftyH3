015832a224f8803f6cc28d2251b1d9afd1e87dc0
pawelmajcher <51097162+pawelmajcher@users.noreply.github.com>
2025-07-28 20:04:25 +0200

    Enable older Swift versions and both Swift 5 and Swift 6 compatibility modes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.10
 
 import PackageDescription
 
@@ -21,5 +21,6 @@ let package = Package(
             name: "SwiftyH3Tests",
             dependencies: ["SwiftyH3"]
         ),
-    ]
+    ],
+    swiftLanguageVersions: [.version("6"), .v5]
 )
